d1c11753a9bcfcaf2c3489e35ad7227defedbda3
IkwelleTochukwu <tochiantho99@gmail.com>
2023-11-13 19:59:13 +0100

    made the Privileges class out of the show_privileges and use as attribute in a class

--- BroCodePython/PythonClasses/PythonClass/users.py
+++ BroCodePython/PythonClasses/PythonClass/users.py
@@ -28,21 +28,34 @@ class Admin(Users):
     def __init__(self, first_name, last_name):
         """Initializes the attribute of the parent class"""
         super().__init__(first_name, last_name)
-        self.privileges = ["can add post", "can delete post", "can ban user"]
+        #self.privileges = ["can add post", "can delete post", "can ban user"]
+        self.privileges = Privileges()
 
-    def show_privileges(self):
+    #def show_privileges(self):
         """Display the privileges"""
-        print(f"The admin user has the privileges; {self.privileges}")
+        #print(f"The admin user has the privileges; {self.privileges}")
+
+class Privileges:
+    """To make a class out of the show_privilege method, and use the class as an attribute"""
+    def __init__(self, privileges=None):
+        if privileges is None:
+            privileges = ["can add post", "can delete post", "can ban user"]
+        self.privileges = privileges
+
+    def describe_privileges(self):
+        """This method will print the privileges to the screen"""
+        print(f'This user can do this: {self.privileges}')
 
 
-#UserInput = Users("Tochukwu", "Ikwelle")
-#UserInput.describe_user()
-#UserInput.greet_user()
-#UserInput.print_login_attempts()
-#UserInput.increment_login_attempts(1)
+
+UserInput = Users("Tochukwu", "Ikwelle")
+UserInput.describe_user()
+UserInput.greet_user()
+UserInput.print_login_attempts()
+UserInput.increment_login_attempts(1)
 #UserInput.print_login_attempts()
 #UserInput.reset_login_attempts()
 #UserInput.print_login_attempts()
 
 Admin = Admin('Tochukwu', 'Ikwelle')
-Admin.show_privileges()
+Admin.privileges.describe_privileges()
